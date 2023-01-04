d009dd27ad848a8c5315c4600fa60ea42dfd8877
Ddoraaaaa <ilbp7024@gmail.com>
2023-01-04 19:13:39 +0700

    done stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 
-const app = express()
+mongoose.connect()
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/bucac', (req, res) => {
+    return res.json({msg:"ditmemay"})
+})
 
 app.listen(3000, () => {
     console.log("tao da song");
