8c8f4cede51ce1238105ce3d291f064dee303d7a
Ddoraaaaa <ilbp7024@gmail.com>
2023-01-04 21:24:55 +0700

    cac

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../services/user.service";
+import { registerUser, findUser } from "../services/user.service";
 
 const router = Router()
 
@@ -9,6 +9,20 @@ router.post('/register', async (req, res) => {
     const result = await registerUser(body);
 
     return res.json({message:"registerrrr succesful", userid:result._id});
+})
+
+router.get('/finduser', async (req, res) =>{
+    const body = req.body;
+
+    const found = await findUser(body);
+    if(!found)
+    {
+        throw new error('NOT_FOUND');
+    }
+    else
+    {
+        return res.json({message:"dude is found", id:found._id});
+    }
 })
 
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userController from "./controllers/user.controller"
+// import contestController from "./controllers/user.controller"
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/codecup')
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(userController);
+// app.use(contestController);
 
 app.listen(3000, () => {
     console.log("tao da song");
