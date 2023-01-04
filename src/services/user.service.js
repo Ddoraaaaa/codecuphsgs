5d83565817b7747a2fdb1dd37b7bb0d880a294e4
Ddoraaaaa <ilbp7024@gmail.com>
2023-01-04 21:00:15 +0700

    stuff

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model";
 
-//register an user
+//register an user, duh
 export async function registerUser(params) {
     const user = await User.create(params);
     return user;
