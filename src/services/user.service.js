36e1d206070ddbb0b05bd606bfde7912ffcce509
Ddoraaaaa <ilbp7024@gmail.com>
2023-01-05 19:56:42 +0700

    abc

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,13 +3,5 @@ import User from "../models/user.model";
 //register an user, duh
 export async function registerUser(params) {
     const user = await User.create(params);
-    console.log(user);
-    return user;
-}
-
-//find user
-export async function findUser(params) {
-    console.log("reached here");
-    const user = await User.findOne(params, (err, obj) => {console.log(obj)});
     return user;
 }
