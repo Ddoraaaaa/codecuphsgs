cc3674d3d85ffc08f26b2478b5381a3990acf1da
hoanggiapvu79116 <hoanggiapvuvhg@gmail.com>
2023-08-10 22:46:40 +1000

    Restyled login & signup pages

--- Frontend/app/authentication/login/page.tsx
+++ Frontend/app/authentication/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { setUserInfo } from "@/session_storage_api/api";
 import {login} from "../../../backend_api/users"
+import Link from "next/link";
 import assert from "assert";
 import {FormEvent, useState} from "react"
 import { useRouter } from "next/navigation";
@@ -54,53 +55,55 @@ export default function LoginPage() {
     }
 
     return (
-        <div className="w-full h-full flex justify-center">
-            <div className="h-full flex-col justify-center">
-                <div className="text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Login</h2>
-                </div>
-                <form id="signup-form" className="text-center mt-2.5">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6">
-                        <div>
-                            <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Username
-                            </label>
-                            <div className="mt-2.5">
-                                <input type="text" name="username" id="username" autoComplete="username" onChange={updateUsername}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </input>
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Email
-                            </label>
-                            <div className="mt-2.5">
-                                <input type="email" name="email" id="email" autoComplete="email" onChange={updateEmail}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </input>
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Password
-                            </label>
-                            <div className="mt-2.5">
-                                <input type="password" name="password" id="password" autoComplete="password" onChange={updatePassword}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </input>
-                            </div>
-                        </div>
-                        <div className="mt-2.5">
-                        <button type="submit" onClick={handleSubmitButtonClicked} 
-                        // do not know why but clicking button still rerender the page even when onSubmit is blocked
-                            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Login
-                        </button>
+        <div className="w-full h-full flex-col justify-center">
+            <div className="text-center">
+                <h2 className="text-3xl font-bold text-gray-900">Login</h2>
+            </div>
+            <form className="mt-4">
+                <div className="grid grid-cols-1 gap-y-4">
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Username
+                        </label>
+                        <div className="mt-1">
+                            <input type="text" name="username" id="username" autoComplete="username" onChange={updateUsername}
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+                            </input>
                         </div>
                     </div>
-                </form>
-            </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-semibold text-gray-900">
+                            Email
+                        </label>
+                        <div className="mt-1">
+                            <input type="email" name="email" id="email" autoComplete="email" onChange={updateEmail}
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+                            </input>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-semibold text-gray-900">
+                            Password
+                        </label>
+                        <div className="mt-1">
+                            <input type="password" name="password" id="password" autoComplete="password" onChange={updatePassword}
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+                            </input>
+                        </div>
+                    </div>
+                    <div className="mt-2.5">
+                        <button type="submit" onClick={handleSubmitButtonClicked} 
+                            className="block w-full rounded-md bg-gray-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                            Login
+                        </button>
+                        <hr className="w-full"></hr>
+                        <div className="w-full text-sm"> 
+                            Don't have an account?&#160;
+                            <Link href="/authentication/signup" className="underline">Register here</Link>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
--- Frontend/app/authentication/signup/page.tsx
+++ Frontend/app/authentication/signup/page.tsx
@@ -5,6 +5,7 @@ import { setUserInfo } from "@/session_storage_api/api";
 import assert from "assert";
 import {FormEvent, useState} from "react"
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 
 export default function SignupPage() { 
@@ -54,53 +55,54 @@ export default function SignupPage() {
     }
 
     return (
-        <div className="bg-white w-full flex content-center justify-center items-center">
-            <div className="flex-col justify-center">
-                <div className="text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Signup</h2>
-                </div>
-                <form id="signup-form" className="text-center mt-2.5">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6">
-                        <div>
-                            <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Username
-                            </label>
-                            <div className="mt-2.5">
-                                <input type="text" name="username" id="username" autoComplete="username" onChange={updateUsername}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </input>
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Email
-                            </label>
-                            <div className="mt-2.5">
-                                <input type="email" name="email" id="email" autoComplete="email" onChange={updateEmail}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </input>
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Password
-                            </label>
-                            <div className="mt-2.5">
-                                <input type="password" name="password" id="password" autoComplete="password" onChange={updatePassword}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </input>
-                            </div>
-                        </div>
-                        <div className="mt-2.5">
-                        <button type="submit" onClick={handleSubmitButtonClicked} 
-                        // do not know why but clicking button still rerender the page even when onSubmit is blocked
-                            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Signup
-                        </button>
+        <div className="bg-white w-full flex-col justify-center items-center">
+            <div className="text-center">
+                <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">Signup</h2>
+            </div>
+            <form id="signup-form" className="mt-4">
+                <div className="grid grid-cols-1 gap-y-4">
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Username
+                        </label>
+                        <div className="mt-1">
+                            <input type="text" name="username" id="username" autoComplete="username" onChange={updateUsername}
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </input>
                         </div>
                     </div>
-                </form>
-            </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Email
+                        </label>
+                        <div className="mt-1">
+                            <input type="email" name="email" id="email" autoComplete="email" onChange={updateEmail}
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </input>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+                            Password
+                        </label>
+                        <div className="mt-1">
+                            <input type="password" name="password" id="password" autoComplete="password" onChange={updatePassword}
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            </input>
+                        </div>
+                    </div>
+                    <div className="mt-1">
+                        <button type="submit" onClick={handleSubmitButtonClicked} 
+                            className="block w-full rounded-md bg-gray-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                            Signup
+                        </button>
+                        <div className="w-full text-sm"> 
+                            Already have an account?&#160;
+                            <Link href="/authentication/login" className="underline">Login here</Link>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
